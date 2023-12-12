5dc3313d62de730553e9f5769712fea0c17e8b77
Eddy <eddy15201@gmail.com>
2023-12-12 16:43:09 +0800

    1. Refactor health api endpoint 2. Add lobby game status api

--- love_letter/web/app.py
+++ love_letter/web/app.py
@@ -2,7 +2,7 @@ import os
 from typing import Union
 
 import uvicorn
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.routing import APIRouter
 from starlette.staticfiles import StaticFiles
@@ -11,6 +11,7 @@ from love_letter.models import GuessCard, ToSomeoneCard
 from love_letter.usecase.create_game import CreateGame
 from love_letter.usecase.get_status import GetStatus
 from love_letter.usecase.join_game import JoinGame
+from love_letter.usecase.lobby_game_status import LobbyGameStatus
 from love_letter.usecase.lobby_start_game import LobbyStartGame
 from love_letter.usecase.play_card import PlayCard
 from love_letter.usecase.start_game import StartGame
@@ -26,6 +27,7 @@ from love_letter.web.presenter import (
     PlayCardPresenter,
     GetStatusPresenter,
     LobbyStartGamePresenter,
+    LobbyGameStatusPresenter,
 )
 
 # isort: on
@@ -114,8 +116,18 @@ async def lobby_start_game(players: LobbyPlayers):
     return presenter.as_view_model()
 
 
-@app.get("/heath")
-async def heath():
+@app.get("/games/{game_id}/status", response_model=GameStatus)
+async def lobby_game_status(request: Request, game_id: str):
+    jwt_token = request.headers.get("Authorization")
+    player_id = JWTBearer.get_player_id(jwt_token)
+    presenter = LobbyGameStatusPresenter.presenter()
+    LobbyGameStatus().execute(LobbyGameStatus.input(game_id, player_id), presenter)
+    game = presenter.as_view_model()
+    return build_player_view(game, player_id)
+
+
+@app.get("/health")
+async def health():
     return {"success": True}
 
 
